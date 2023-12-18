49eedb070c5805869b8e8aff32efb9439d6f058b
darshana5125 <darshana5125@gmail.com>
2023-12-18 08:04:23 +0530

    darshana branch

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ReviewsController;
