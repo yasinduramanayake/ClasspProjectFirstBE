008103e9f23b95a426665bcd2143012f5ca9a286
Yasindu <yasindu@sp-solutions.biz>
2023-12-31 21:35:14 +0530

    updted

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
-use App\Repositary\testinterface;
+use App\UserRepositary\testinterface;
 use App\Http\Controllers\Exception;
 use Exception as GlobalException;
 
