dcb33bb610d94cf3a69f226ded4add2e789027a9
dhanushika <dhanushika76@gmail.com>
2023-12-31 19:20:57 +0530

    update category repository

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,8 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
+use App\CategoryRepository\categoryinterface;
+use App\CategoryRepository\categoryimplementation;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,6 +15,7 @@ class AppServiceProvider extends ServiceProvider
     public function register(): void
     {
         //
+        $this->app->bind(categoryinterface::class, categoryimplementation::class);
     }
 
     /**
