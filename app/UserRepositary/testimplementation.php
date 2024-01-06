e8006009558874ecc7e0602b2859a9eddbdf6bb6
Yasindu <yasindu@sp-solutions.biz>
2024-01-06 15:08:15 +0530

    updated

--- app/UserRepositary/testimplementation.php
+++ app/UserRepositary/testimplementation.php
@@ -1,21 +1,21 @@
 <?php
 
 namespace App\UserRepositary;
+
 use App\UserRepositary\testinterface;
 use App\Models\User;
 
 class testimplementation implements testinterface
 {
-    public function store($data){
-        $createddata= User::create($data);
+    public function store($data)
+    {
+        $createddata = User::create($data);
         return $createddata;
-
     }
 
-    public function index(){
-        $alltestdata=User::all();
+    public function index()
+    {
+        $alltestdata = User::all();
         return $alltestdata;
-
     }
-
 }
